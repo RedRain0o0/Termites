c0dccb95e56cbcd04eb73aacbcd61f3c5ea816fb
RedRain0o0 <redraino.o.o.o.o@gmail.com>
2023-08-08 14:41:27 -0400

    Initial commit

---
+++ src/main/java/io/github/redrain0o0/termites/TermitesClient.java
@@ -0,0 +1,10 @@
+package io.github.redrain0o0.termites;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TermitesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
